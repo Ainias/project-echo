5a4256d04be3665540eb4f9aed4251b33a9054ae
silas <silas@silas.link>
2019-08-18 17:47:32 +0200

    update

--- tools/release-apk.sh
+++ tools/release-apk.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-npm run "cordova browser prepare"
-rm -rf server/public
-mv platforms/browser/www server/public
-
 #npm run "cordova release android"
 #mv platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk server/public/echo.apk
