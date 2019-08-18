f76ee4b3f77a431cf9c588e6034c380bf180885e
silas <silas@silas.link>
2019-08-18 16:43:14 +0200

    updated release

--- tools/build-web.sh
+++ tools/build-web.sh
@@ -3,3 +3,6 @@
 npm run "cordova browser prepare"
 rm -rf server/public
 mv platforms/browser/www server/public
+
+npm run "cordova release android"
+mv platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk server/public/echo.apk
