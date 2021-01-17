fb0297915c39cd1f392529d18bd8a35ea7c805a2
silas <silas@silas.link>
2021-01-17 12:16:27 +0100

    new version

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -3,6 +3,7 @@
 export GOOGLE_MAPS_API_KEY=AIzaSyCVzdiXCm6e-qGVjl_a2-As3Y6z9vyJt_k
 export MODE=production
 export HOST_URI=https://echoapp.de
+#export HOST_URI=http://127.0.0.1:3000
 export CONTACT_EMAIL=kontakt@echoapp.de
 
 npm run "cordova browser prepare"
