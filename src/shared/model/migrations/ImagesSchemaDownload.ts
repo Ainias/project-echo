94f0dd5d23cae1bc8552d0e6bfaf9e024cf0eb6b
silas <silas@silas.link>
2021-02-17 18:12:38 +0100

    bugfix, changed expand-symvol

--- src/shared/model/migrations/ImagesSchemaDownload.ts
+++ src/shared/model/migrations/ImagesSchemaDownload.ts
@@ -10,12 +10,13 @@ export class ImagesSchemaDownload1000000011000 implements MigrationInterface {
             await MigrationHelper.updateModel(queryRunner, FileMedium);
             await queryRunner.query("SET foreign_key_checks=1;");
         } else {
-            console.log("ImagesSchemaDownload 1")
-            await queryRunner.query("PRAGMA foreign_keys=OFF;");
-            console.log(await queryRunner.query("PRAGMA foreign_keys"));
+            //Why ever stop und start von Transaction, damit foreign keys funktionieren
+            await queryRunner.commitTransaction();
+            await queryRunner.startTransaction();
+
+            await queryRunner.query("PRAGMA foreign_keys = OFF");
             await MigrationHelper.updateModel(queryRunner, FileMedium);
             await queryRunner.query("PRAGMA foreign_keys = ON");
-            console.log("ImagesSchemaDownload 2")
         }
     }
 
